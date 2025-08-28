b523717e3e01ef9f19322aa66de5e8599b338382
SblakeUNA <163796752+SblakeUNA@users.noreply.github.com>
2025-08-28 13:16:48 -0500

    Update app.mjs

--- app.mjs
+++ app.mjs
@@ -1,31 +1,30 @@
 import express from 'express'
-import path from 'path';
-// import { dirname } from 'node:path';
-// import { fileURLToPath } from 'node:url';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
 const app = express()
 const PORT = process.env.PORT || 3000; 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-app.use(express.static(path.join(__dirname, 'public')))
-
-// app.use(express.static(path.join(__dirname, 'public'))); // Serve static files
-
-
-// app.use(express.static(__dirname + 'public'));
+app.use(express.static(join(__dirname, 'public')));
 
 
 app.get('/', (req, res) => {
-  res.send('Hello Express from Render. <a href="spencer">Spencer</a>')
+  res.send('Hello Express from Render 😍😍😍. <a href="barry">barry</a>')
 })
 
 // endpoints...middlewares...apis? 
 // send an html file
 
-app.get('/spencer', (req, res) => {
+app.get('/barry', (req, res) => {
   // res.send('barry. <a href="/">home</a>')
+  res.sendFile(join(__dirname, 'public', 'barry.html')) 
 
-  res.sendFile('spencer.html'); 
+})
 
+
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
 })
