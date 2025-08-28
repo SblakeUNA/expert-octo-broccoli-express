62c03de822fb3f3deb7e288abf6c5d3b4708e814
SblakeUNA <163796752+SblakeUNA@users.noreply.github.com>
2025-08-28 12:41:55 -0500

    Update app.mjs

--- app.mjs
+++ app.mjs
@@ -1,9 +1,31 @@
 import express from 'express'
+import path from 'path';
+// import { dirname } from 'node:path';
+// import { fileURLToPath } from 'node:url';
 
 const app = express()
+const PORT = process.env.PORT || 3000; 
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+// app.use(express.static(path.join(__dirname, 'public'))); // Serve static files
+
+
+// app.use(express.static(__dirname + 'public'));
+
 
 app.get('/', (req, res) => {
-  res.send('Hello Express')
+  res.send('Hello Express from Render. <a href="spencer">Spencer</a>')
 })
 
-app.listen(3000)
+// endpoints...middlewares...apis? 
+// send an html file
+
+app.get('/spencer', (req, res) => {
+  // res.send('barry. <a href="/">home</a>')
+
+  res.sendFile('spencer.html'); 
+
+})
